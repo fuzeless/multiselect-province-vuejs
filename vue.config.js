ee8ff1f81463013d97bf207135e19fc20d64148d
fuzeless <lequangnhat2000@gmail.com>
2022-06-15 17:58:35 +0700

    Add SCSS preloaders

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,11 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "/src/styles/_variables.scss";',
+      },
+    },
+  },
 });
